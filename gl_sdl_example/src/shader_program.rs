79d124267fc07b5b3b90e758f204e38a8a854e0b
Gavin J Stark <atthecodeface@gmail.com>
2024-08-07 17:05:47 +0100

    gl_sdl_example: Moved to use create_pipeline instead of compile/link shader

--- gl_sdl_example/src/shader_program.rs
+++ gl_sdl_example/src/shader_program.rs
@@ -11,11 +11,12 @@ pub fn create_shader_program<G: Gl>(
     shader_paths: &[&Path],
 ) -> Result<G::Program, anyhow::Error> {
     let shader = read_file(shader_paths, shader_filename)?;
-    let shader: mod3d_gl::ShaderProgramDesc = serde_json::from_str(&shader)?;
-    let shader_program = shader
-        .compile(gl, &|filename| {
-            read_file(shader_paths, filename).map_err(|e| e.to_string())
-        })
+    let shader: G::PipelineDesc<'_> = serde_json::from_str(&shader)?;
+    let shader_program = gl
+        .create_pipeline(
+            &|filename| read_file(shader_paths, filename).map_err(|e| e.to_string()),
+            Box::new(shader),
+        )
         .map_err(|e| anyhow::anyhow!("Failed to read shader program {e}"))?;
 
     Ok(shader_program)
